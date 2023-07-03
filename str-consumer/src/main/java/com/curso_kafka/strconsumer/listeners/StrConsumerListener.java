0208b70be54e511cfcec4f8c439983ab2ecb0a3d
Gabriel593-dotcom <g.dos.pinheiro1999@gmail.com>
2023-07-03 16:38:19 -0300

    retirando boilerplate code com @interface assinada

--- str-consumer/src/main/java/com/curso_kafka/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/curso_kafka/strconsumer/listeners/StrConsumerListener.java
@@ -1,32 +1,21 @@
 package com.curso_kafka.strconsumer.listeners;
 
-import lombok.extern.log4j.Log4j2;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
+
+import com.curso_kafka.strconsumer.custom.StrConsumerCustomListener;
+
+import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
-  @KafkaListener(
-    groupId = "group-1",
-    topicPartitions = {
-      @TopicPartition(topic = "str-topic", partitions = { "0" }),
-    },
-    containerFactory = "strContainerFactory"
-  )
+  @StrConsumerCustomListener(groupId = "group-1")
   public void create(String message) {
     log.info("CREATE ::: Receive message {}", message);
   }
 
-  @KafkaListener(
-    groupId = "group-1",
-    topicPartitions = {
-      @TopicPartition(topic = "str-topic", partitions = { "1" }),
-    },
-    containerFactory = "strContainerFactory"
-  )
+  @StrConsumerCustomListener(groupId = "group-1")
   public void log(String message) {
     log.info("LOG ::: Receive message {}", message);
   }
