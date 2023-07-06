f73bc4c9c78649fe093392b026cc59014f052861
Gabriel593-dotcom <g.dos.pinheiro1999@gmail.com>
2023-07-06 14:36:24 -0300

    record interceptor implementation

--- str-consumer/src/main/java/com/curso_kafka/strconsumer/config/StringConsumerConfig.java
+++ str-consumer/src/main/java/com/curso_kafka/strconsumer/config/StringConsumerConfig.java
@@ -10,9 +10,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.RecordInterceptor;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @RequiredArgsConstructor
 @Configuration
 public class StringConsumerConfig {
@@ -34,5 +37,24 @@ public class StringConsumerConfig {
         factory.setConsumerFactory(consumerFactory);
         return factory;
     }
-    
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> validMessageContainerFactory(
+        ConsumerFactory<String, String> consumerFactory
+    ) {
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.setRecordInterceptor(validMessage());
+        return factory;
+    }
+
+    private RecordInterceptor<String, String> validMessage() {
+        return record -> {
+            if(record.value().contains("teste")) {
+                log.info("Possui a palavra teste.");
+                return record;
+            }
+            return record; 
+        };
+    }
 }
--- str-consumer/src/main/java/com/curso_kafka/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/curso_kafka/strconsumer/listeners/StrConsumerListener.java
@@ -1,10 +1,9 @@
 package com.curso_kafka.strconsumer.listeners;
 
-import org.springframework.stereotype.Component;
-
 import com.curso_kafka.strconsumer.custom.StrConsumerCustomListener;
-
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
@@ -18,5 +17,14 @@ public class StrConsumerListener {
   @StrConsumerCustomListener(groupId = "group-1")
   public void log(String message) {
     log.info("LOG ::: Receive message {}", message);
+  }
+
+  @KafkaListener(
+    groupId = "group-2",
+    containerFactory = "validMessageContainerFactory",
+    topics = "str-topic"
+  )
+  public void history(String message) {
+    log.info("HISTORY ::: receive message {}", message);
   }
 }
